a5f85cd588ef58e68884c844f95a3eef10c5d26b
GIOVANNI <GIOVANNI@DESKTOP-975IEBI>
2022-08-21 22:39:21 -0500

    subiendo proyecto algoritmos

---
+++ src/semana01/Ejer01.java
@@ -0,0 +1,12 @@
+
+package semana01;
+
+/**
+ *
+ * @author GIOVANNI
+ */
+public class Ejer01 {
+    public static void main(String[] args) {
+        System.out.println("HOLA MUNDO DE JAVA....JJJEIEJJE HASTA EL AGUJERO NEGRO ");
+    }
+}
---
+++ src/semana03/Ejer01.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package semana03;
+
+/**
+ *jdk  compilador (javac) java jit
+ * @author GIOVANNI
+ */
+public class Ejer01 {
+//accesos(encapsulamiento)
+//PUBLIC TODOS
+//PRIVATE SOLO LA CALSE
+//PROTECTED ERENCIA
+//DEFAULT  SOLO EL PAQUETE
+    public static void main(String[] args) {
+       
+    }
+    
+}
+class Perro{
+
+}
