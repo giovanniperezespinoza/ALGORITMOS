b02627ec8c47625b2dd2effde5059eb5dec975da
GIOVANNI <GIOVANNI@DESKTOP-975IEBI>
2022-08-21 22:58:07 -0500

    algoritmos

--- src/semana01/Ejer01.java
+++ src/semana01/Ejer01.java
@@ -9,5 +9,5 @@ public class Ejer01 {
     public static void main(String[] args) {
         System.out.println("HOLA MUNDO DE JAVA....JJJEIEJJE HASTA EL AGUJERO NEGRO ");
     }
- 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
 }
+//se esta visualizando
