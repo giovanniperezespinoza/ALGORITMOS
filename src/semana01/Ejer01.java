a218ba6ef61db47f420591ce620c18f509985803
GIOVANNI <GIOVANNI@DESKTOP-975IEBI>
2022-08-21 23:04:59 -0500

    algoritmos

--- src/semana01/Ejer01.java
+++ src/semana01/Ejer01.java
@@ -6,6 +6,7 @@ package semana01;
  * @author GIOVANNI
  */
 public class Ejer01 {
+  //primera semana de la clase
     public static void main(String[] args) {
         System.out.println("HOLA MUNDO DE JAVA....JJJEIEJJE HASTA EL AGUJERO NEGRO ");
     }
--- src/semana03/Ejer01.java
+++ src/semana03/Ejer01.java
@@ -20,5 +20,5 @@ public class Ejer01 {
     
 }
 class Perro{
-
+//todo codigo
 }
